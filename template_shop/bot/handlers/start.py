df69c0f2409232bb92b84b7db112fedc17a6119e
flaskuser1 <rwummkucd@laste.ml>
2025-01-10 20:14:47 +0200

    Added updating stats on bot start

--- template_shop/bot/handlers/start.py
+++ template_shop/bot/handlers/start.py
@@ -3,10 +3,13 @@ from aiogram_dialog import DialogManager, StartMode
 
 from template_shop.bot.states import ClientSG
 from template_shop.core.exceptions.user import UserTgIdNotExist
+from template_shop.infrastructure.database.services.statistics import StatsService
 from template_shop.infrastructure.database.services.user import UserService
 
 
-async def user_start(message: types.Message, dialog_manager: DialogManager, user_service: UserService):
+async def user_start(
+    message: types.Message, dialog_manager: DialogManager, user_service: UserService, stats_service: StatsService
+):
     """Команда старта для пользователей"""
 
     user_id = message.from_user.id
@@ -20,5 +23,6 @@ async def user_start(message: types.Message, dialog_manager: DialogManager, user
             telegram_id=user_id,
             username=username,
         )
+        await stats_service.add_stats(users_registered=1)
 
     await dialog_manager.start(ClientSG.start, mode=StartMode.RESET_STACK)
